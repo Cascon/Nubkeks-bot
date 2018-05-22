73f1e3c45973f47f71a720b097e31c137ac0578d
Reberto33 <39516545+Reberto33@users.noreply.github.com>
2018-05-22 16:47:00 +0100

    twitch announce
    all done except ping and emote

--- commands/stream.js
+++ commands/stream.js
@@ -10,7 +10,7 @@ module.exports.run = async(bot, message, args) => {
   }, (error, response, body) => {
     try {
     if (!error && response.body.stream !== null) {
-      return message.channel.send("Nubkeks is currently live over at https://www.twitch.tv/nubkeks")
+      return message.channel.send("Nubkeks is currently live over at https://www.twitch.tv/nubkeks");
     } else if (response.body.stream === null){
       return message.channel.send("Stream is not currently live. Most weekdays (Monday-Friday) Nubkeks will be streaming at 1pm CEST | 6am PT for a good few hours ----- Nubkeks also streams every Friday at 10pm GMT | 3pm PT on the TGN Squadron youtube channel! ");
     }
---
+++ commands/twitchAnnouncement.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+const request = require("request");
+const schedule = require("node-schedule");
+
+// Sets up the timer to auto run every 3 minutes (recommended time amount)
+var announcementLoop = schedule.scheduleJob('* */3 * * * *', () => {
+  module.exports.run = async(bot, message, args) => {
+    // Request the JSON data from the Twitch API for nubs channel
+    request ({
+      url: 'https://api.twitch.tv/kraken/streams/nubkeks?client_id=wgmissje9l67nel3xwxurzureger8k',
+      json: true
+    }, (error, response, body) => {
+      // Set the boolean variable
+      var postedAnnouncement = false;
+      if (!error && response.body.stream !== null && postedAnnouncement === false) {
+        // Post announcement
+        return message.channel.send("@here One and all nub is live over here! :PogChamp: :point_right: https://www.twitch.tv/nubkeks");
+        // Set boolean to true when announcement has been posted to avoid repitition
+        postedAnnouncement = true;
+      } else if (response.body.stream === null && postedAnnouncement === true){
+        // Set boolen to false when nub goes offline so as to allow for new announcement when he goes live
+        postedAnnouncement = false;
+      };
+    });
+  };
+});
+
+module.exports.help = {
+	name: "twitchAnnouncement"
+}
